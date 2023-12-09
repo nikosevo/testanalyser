851afdad6dde1bbba158bfc3c3e697361e205f4a
Nikos Tzekas <48737084+nikosevo@users.noreply.github.com>
2023-12-09 14:27:02 +0100

    Create test.c

---
+++ test.c
@@ -0,0 +1,16 @@
+
+int main(){
+
+    printf("%d",sizeof(short));
+
+    int a = 23293874293847293847239847239847; //thats obviously too long
+
+    int* p;
+
+    *p = 4;
+
+
+    if(i < 3){
+        return;
+    }
+}
